ea45c2850569a4a28f6327e2b5d61185488e9a64
Zach <zmommaerts@gmail.com>
2022-02-02 20:37:58 -0600

    added orbit controls and lights

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,28 @@
 import './App.css';
-import { Canvas } from '@react-three/fiber'
+import { Canvas } from '@react-three/fiber';
 import { Suspense } from "react";
+import { OrbitControls} from '@react-three/drei';
+import styled from 'styled-components';
 import Scene from './Scene';
+import { AmbientLight } from 'three';
+
+const Div = styled.div`
+  width: 100%;
+  height: 100%;
+`;
 
 function App() {
   return (
-    <Canvas>
-      <Suspense fallback={null}>
-        <Scene />
-      </Suspense>
-    </Canvas>
+    <Div>
+      <Canvas>
+        <OrbitControls />
+        <ambientLight  intensity={1}/>
+        <directionalLight  intensity={0.8}/>
+        <Suspense fallback={null}>
+          <Scene />
+        </Suspense>
+      </Canvas>
+    </Div>
   );
 }
 
